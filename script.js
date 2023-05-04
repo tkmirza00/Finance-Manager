c9272f414c8d7cac2561d3ce0874388b18edd690
tkmirza00 <88179245+tkmirza00@users.noreply.github.com>
2023-05-05 02:11:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,169 @@
+const incomeSection = document.querySelector(".income-area");
+const expensesSection = document.querySelector(".expenses-area");
+const availableMoney = document.querySelector(".available-money");
+const addTransactionPanel = document.querySelector(".add-transaction-panel");
+
+const nameInput = document.querySelector("#name");
+const amountInput = document.querySelector("#amount");
+const categorySelect = document.querySelector("#category");
+
+const addTransactionBtn = document.querySelector(".add-transaction");
+const saveBtn = document.querySelector(".save");
+const cancelBtn = document.querySelector(".cancel");
+const deleteBtn = document.querySelector(".delete");
+const deleteAllBtn = document.querySelector(".delete-all");
+
+const lightStyleBtn = document.querySelector(".light");
+const darkStyleBtn = document.querySelector(".dark");
+
+let root = document.documentElement;
+let ID = 0;
+let categoryIcon;
+let selectedCategory;
+let moneyArr = [0];
+
+const showPanel = () => {
+    addTransactionPanel.style.display = "flex";
+};
+
+const closePanel = () => {
+    addTransactionPanel.style.display = "none";
+    clearInputs();
+};
+
+const checkForm = () => {
+    if (
+        nameInput.value !== "" &&
+        amountInput.value !== "" &&
+        categorySelect.value !== "none"
+    ) {
+        createNewTransaction();
+    } else {
+        alert("Fill in all fields!");
+    }
+};
+
+const clearInputs = () => {
+    nameInput.value = "";
+    amountInput.value = "";
+    categorySelect.selectedIndex = 0;
+};
+
+const createNewTransaction = () => {
+    const newTransaction = document.createElement("div");
+    newTransaction.classList.add("transaction");
+    newTransaction.setAttribute("id", ID);
+
+    checkCategory(selectedCategory);
+
+    newTransaction.innerHTML = `
+        <p class="transaction-name">${categoryIcon} ${nameInput.value}</p>
+        <p class="transaction-amount">${amountInput.value}$
+        <button class="delete" onClick="deleteTransaction(${ID})"><i class="fas fa-times"></i></button></p>
+        `;
+
+    amountInput.value > 0
+        ? incomeSection.appendChild(newTransaction) &&
+          newTransaction.classList.add("income")
+        : expensesSection.appendChild(newTransaction) &&
+          newTransaction.classList.add("expense");
+
+    moneyArr.push(parseFloat(amountInput.value));
+    countMoney(moneyArr);
+
+    closePanel();
+    ID++;
+    clearInputs();
+};
+
+const selectCategory = () => {
+    selectedCategory =
+        categorySelect.options[categorySelect.selectedIndex].text;
+};
+
+const checkCategory = (category) => {
+    switch (category) {
+        case "[ + ] 8.8kw sales":
+            categoryIcon = '<i class="fas fa-money-bill-wave"></i>';
+            break;
+        case "[ + ] 500kw sales":
+            categoryIcon = '<i class="fas fa-money-bill-wave"></i>';
+            break;
+        case "[ + ] 1880kw sales":
+            categoryIcon = '<i class="fas fa-money-bill-wave"></i>';
+            break;
+        case "[ + ] Engine parts sales":
+            categoryIcon = '<i class="fas fa-money-bill-wave"></i>';
+            break;                
+        case "[ - ] Material costs":
+            categoryIcon = '<i class="fas fa-cart-arrow-down"></i>';
+            break;
+        case "[ - ] Warehouse Rent":
+            categoryIcon = '<i class="fas fa-hamburger"></i>';
+            break;
+        case "[ - ] Construction cost":
+            categoryIcon = '<i class="fas fa-film"></i>';
+            break;
+        case "[ - ] Manager salary e.g production, h.r, onsite engineers":
+            categoryIcon = '<i class="fas fa-film"></i>';
+            break;
+        case "[ - ] Machinist salary":
+            categoryIcon = '<i class="fas fa-film"></i>';
+            break;
+        case "[ - ] SCR outsourcing":
+            categoryIcon = '<i class="fas fa-film"></i>';
+            break;
+        case "[ - ] Planning Outsourcing":
+            categoryIcon = '<i class="fas fa-film"></i>';
+            break;
+
+    }
+};
+
+
+const countMoney = (money) => {
+    const newMoney = money.reduce((a, b) => a + b);
+    availableMoney.textContent = `${newMoney}$`;
+};
+
+const deleteTransaction = (id) => {
+    const transactionToDelete = document.getElementById(id);
+    const transactionAmount = parseFloat(
+        transactionToDelete.childNodes[3].innerText
+    );
+    const indexOfTransaction = moneyArr.indexOf(transactionAmount);
+
+    moneyArr.splice(indexOfTransaction, 1);
+
+    transactionToDelete.classList.contains("income")
+        ? incomeSection.removeChild(transactionToDelete)
+        : expensesSection.removeChild(transactionToDelete);
+
+    countMoney(moneyArr);
+};
+
+const deleteAllTransactions = () => {
+    incomeSection.innerHTML = "<h3>Income:</h3>";
+    expensesSection.innerHTML = "<h3>Expenses:</h3>";
+    availableMoney.textContent = "0$";
+    moneyArr = [0];
+};
+
+const changeStyleToLight = () => {
+    root.style.setProperty("--first-color", "#f9f9f9");
+    root.style.setProperty("--second-color", "#14161f");
+    root.style.setProperty("--border-color", "rgba(0, 0, 0, 0.2)");
+};
+
+const changeStyleToDark = () => {
+    root.style.setProperty("--first-color", "#14161f");
+    root.style.setProperty("--second-color", "#f9f9f9");
+    root.style.setProperty("--border-color", "rgba(255, 255, 255, 0.4)");
+};
+
+addTransactionBtn.addEventListener("click", showPanel);
+cancelBtn.addEventListener("click", closePanel);
+saveBtn.addEventListener("click", checkForm);
+deleteAllBtn.addEventListener("click", deleteAllTransactions);
+lightStyleBtn.addEventListener("click", changeStyleToLight);
+darkStyleBtn.addEventListener("click", changeStyleToDark);
